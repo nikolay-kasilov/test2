5ccd712309fd205f792131978525a6541e456983
nikolay-kasilov <pekolian-dengi@yandex.ru>
2024-03-20 21:36:20 +0300

    21_35

--- utils.py
+++ utils.py
@@ -1,22 +1,19 @@
 import json
 
-PATH = 'tasks.json'
+PATH = "tasks.json"
 
 
 def write_tasks(tasks: list[dict[str, str | int]], path=PATH):
-    with open(path, 'w', encoding='utf-8') as f:
+    with open(path, "w", encoding="utf-8") as f:
         json.dump(tasks, f, ensure_ascii=False, indent=4)
 
 
 def read_tasks(path=PATH) -> list[dict[str, str | int]]:
-    with open(path, 'r', encoding='utf-8') as f:
+    with open(path, "r", encoding="utf-8") as f:
         return json.load(f)
 
 
-def filter_tasks(tasks: list[dict[str, str | int]], priority: int) \
-        -> list[dict[str, str | int]]:
-    return [
-        task
-        for task in tasks
-        if task.get('priority') == priority
-    ]
+def filter_tasks(
+    tasks: list[dict[str, str | int]], priority: int
+) -> list[dict[str, str | int]]:
+    return [task for task in tasks if task.get("priority") == priority]
